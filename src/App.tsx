78cd59c69c3292efb782273ad0951c6d2544d3cb
Piotrek <piotrek380@gmail.com>
2021-06-22 19:31:33 +0200

    Add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <button type="button" style={{ color: 'red', backgroundColor: 'green' }}>
+          Click me bro
+        </button>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
