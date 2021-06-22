be5f531b71f55b42ca16fc35f30a388206bcc82d
Piotrek <piotrek380@gmail.com>
2021-06-22 20:44:35 +0200

    Install Styled-Componenets and edit tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 
-const btnStyles = {
-  color: 'red',
-  backgroundColor: 'green',
-};
+import { AppContainer, ColumnContainer, ColumnTitle } from './styles';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <button type="button" style={btnStyles}>
-          Click me bro
-        </button>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <ColumnContainer>
+        <ColumnTitle>
+          <h2>ELO</h2>
+        </ColumnTitle>
+        <ColumnTitle>
+          <h2>ELO#2</h2>
+        </ColumnTitle>
+      </ColumnContainer>
+      <ColumnContainer>
+        <ColumnTitle>
+          <h2>ELO</h2>
+        </ColumnTitle>
+        <ColumnTitle>
+          <h2>ELO#2</h2>
+        </ColumnTitle>
+      </ColumnContainer>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+    </AppContainer>
   );
 };
 
---
+++ src/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+  align-items: flex-start;
+  background-color: #3179ba;
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  padding: 20px;
+  width: 100%;
+`;
+
+export const ColumnContainer = styled.div`
+  background-color: red;
+`;
+
+export const ColumnTitle = styled.div`
+  background-color: red;
+`;
+
+export const CardContainer = styled.div`
+  background-color: red;
+`;
